7ecdc6f62b59bff7c9ff34ca99405349d8ff155d
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2025-10-20 23:36:02 +0200

    Fix bootstraping class webpack runtiime error and update tsconfig by setting types to node, also add new jest setupTest config file to ensure test are not causing errors due to type=node set in tsconfig

--- core/src/init-cli/generate.project.ts
+++ core/src/init-cli/generate.project.ts
@@ -15,7 +15,6 @@ import { AbimongoConfig, ProjectOptions } from '../types';
 
 export async function generateProject(options: AbimongoConfig) {
   const projectRoot = path.resolve(process.cwd(), options.projectName || 'abimongo-app');
-  // const allFoldersAndFiles = foldersAndFiles();
   const { generatedFolders, generatedFiles } = foldersAndFiles();
 
   setInterval(() => {
@@ -48,9 +47,6 @@ export async function generateProject(options: AbimongoConfig) {
   // Log generated folders and files
   generatedFolders();
   generatedFiles();
-
-  // allFoldersAndFiles.generatedFolders();
-  // allFoldersAndFiles.generatedFiles();
 
   console.log(chalk.green.bold('[Completed]: \nProject generation completed successfully!'));
 }
